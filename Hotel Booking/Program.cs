0499ee7e45efe2295146eada735eade88f842909
Aruden <84431560+ArudenKun@users.noreply.github.com>
2023-03-10 14:32:47 +0800

    Fixes

--- Hotel Booking/Program.cs
+++ Hotel Booking/Program.cs
@@ -1,6 +1,9 @@
+using System;
+using System.Net.Http;
 using Hotel_Booking;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
